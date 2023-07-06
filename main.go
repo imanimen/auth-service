fd1b21313f8579d4bf40dd0a2a602122cd74e1c5
iman <imanimen81@gmail.com>
2023-07-06 22:00:36 +0330

    REFACTOR main code

--- main.go
+++ main.go
@@ -12,20 +12,20 @@ var SECRET = []byte("super-secret-auth-key")
 
 var api_key = "1234"
 
-func validateJwt(next func(w http.ResponseWriter, r* http.Request)) http.Handler {
-	return http.HandlerFunc( func(w http.ResponseWriter, r *http.Request) {
-		if r.Header["token"] != nil {
-			token, err := jwt.Parse(r.Header["token"][0], func(t *jwt.Token) (interface{}, error) {
-				_, ok := t.Method.(*jwt.SigningMethodHMAC)
-				if !ok {
-					w.WriteHeader(http.StatusUnauthorized)
-					w.Write([]byte("Unauthorized"))
+func validateJwt(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Authorization") != "" {
+			tokenString := r.Header.Get("Authorization")[7:]
+			token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+				if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
 				}
-				return SECRET, nil
+				return []byte(SECRET), nil
 			})
 			if err != nil {
 				w.WriteHeader(http.StatusUnauthorized)
-				w.Write([]byte("Unauthorized " + err.Error() ))
+				w.Write([]byte("Unauthorized " + err.Error()))
+				return
 			}
 			if token.Valid {
 				next(w, r)
@@ -33,14 +33,17 @@ func validateJwt(next func(w http.ResponseWriter, r* http.Request)) http.Handler
 				w.WriteHeader(http.StatusUnauthorized)
 				w.Write([]byte("Unauthorized"))
 			}
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			w.Write([]byte("Unauthorized"))
 		}
-	})
+	}
 }
 
 func createJwt() (string, error) {
-	token := jwt.New(jwt.SigningMethodHS384)
-	claims := token.Claims.(jwt.MapClaims)
-	claims["exp"] = time.Now().Add(time.Hour).Unix()
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"exp": time.Now().Add(time.Hour).Unix(),
+	})
 	tokenStr, err := token.SignedString(SECRET)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -49,22 +52,21 @@ func createJwt() (string, error) {
 	return tokenStr, nil
 }
 
-
-
 func getJwt(w http.ResponseWriter, r *http.Request) {
-	if r.Header["api"] != nil {
-		if r.Header["api"][0] == api_key {
-			token, err := createJwt()
-			if err != nil {
-				return 
-			}
-			fmt.Fprint(w, token)
+	if r.Header.Get("api") == api_key {
+		token, err := createJwt()
+		if err != nil {
+			return
 		}
+		fmt.Fprint(w, token)
+	} else {
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte("Unauthorized"))
 	}
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "seper secret api")
+	fmt.Fprintf(w, "super secret api")
 }
 
 func main() {
